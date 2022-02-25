ea9e1787e2d4228786e9499e337d85dea6251b19
haruharu9000 <nitori4baime@gmail.com>
2022-02-26 00:00:18 +0900

    進捗

--- lib/github_oauth_credentials.dart
+++ lib/github_oauth_credentials.dart
@@ -1,6 +1,6 @@
 // TODO(CodelabUser): Create an OAuth App
-const githubClientId = 'YOUR_GITHUB_CLIENT_ID_HERE';
-const githubClientSecret = 'YOUR_GITHUB_CLIENT_SECRET_HERE';
+const githubClientId = '114009a48f5733029b4f';
+const githubClientSecret = 'c4bc42cf532102def5815f1ec1dfa2dddc30dfda';
 
 // OAuth scopes for repository and user information
 const githubScopes = ['repo', 'read:org'];
